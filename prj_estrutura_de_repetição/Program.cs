372ef0681f33a65ff5b361ebc922b3a7b6e93600
Yasmin Tavares <134327320+yaasmintavares@users.noreply.github.com>
2024-04-19 13:54:49 -0300

    Add files via upload
    Estrutura de Repetição : Loop ou Laço - Utilizando o comando FOR (Para ... Fim-Para)
    
    Exibindo dez vezes seu nome na tela com o uso da estrutura Par ... Fim-para (For)

---
+++ prj_estrutura_de_repetição/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj_estrutura_de_repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            for (c = 1; c <= 10; c++)
+            {
+                Console.WriteLine("Yasmin de Oliveira Tavares \n");
+            }
+            Console.WriteLine("\nPress. <ENTER> para Finalizar\n");
+            Console.ReadKey();
+        }
+    }
+}
